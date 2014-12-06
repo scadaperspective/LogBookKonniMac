8ed9de783fa0568772a250721b19109edbfebc9d
Del <del@babel.com.au>
2014-12-06 15:45:59 +0700

    Fix typos

--- makezip.sh
+++ makezip.sh
@@ -5,33 +5,26 @@
 # See the comments in cmake/PluginLocalization.cmake
 #
 
+mkdir -p build
+
 #
 # Languages zip contains the language resource files.  Build the plugin
 # first using cmake before creating this file.  We build this file anyway
 # even though the languages are included in the packages just in case
 # there are changes needed.
 #
-rm -f LogbookKonni_Languages.zip
+rm -f build/LogbookKonni_Languages.zip
 (
     cd build/Resources
-    zip -r ../../LogbookKonni_Languages *
+    zip -r ../LogbookKonni_Languages *
 )
 
 #
 # Layouts zip contains the layouts used when printing or saving the logbook
-# to HTML or ODT file format.
+# to HTML or ODT file format. It also contains the help.
 #
-rm -f LogbookKonni_Layouts.zip
+rm -f build/LogbookKonni_Layouts.zip
 (
     cd data
-    zip -r ../LogbookKonni_Layouts.zip HTMLLayouts ODTLayouts
-)
-
-#
-# Others zip file contains the help.
-#
-rm -f LogbookKonni_Other.zip
-(
-    cd data
-    zip -r ../LogbookKonni_Other.zip Help.html Clouds Images
+    zip -r ../build/LogbookKonni_Layouts.zip HTMLLayouts ODTLayouts Help.html Clouds Images
 )
